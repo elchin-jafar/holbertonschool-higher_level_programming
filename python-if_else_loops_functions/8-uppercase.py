93400433f90e1d81a763c926833f72e71f62f8b7
Elchin <elcinceferli@gmail.com>
2024-01-27 15:14:51 -0800

    add string formatter coz its requierement

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for char in str:
         if ord(char) > 96 and ord(char) < 123:
             char = chr(ord(char) - 32)
-        print(char, end="")
+        print("{}".format(char), end="")
     print("")
