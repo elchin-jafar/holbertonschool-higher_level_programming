c5d99a51d96df006e19512d6a990a4135ed35c1c
Elchin <elcinceferli@gmail.com>
2024-01-27 15:49:11 -0800

    change if statement

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if(i == 101 or i == 113):
+for number in range(97, 123):
+    if number == 101 or number == 113:
         continue
-    print("{}".format(chr(i)), end="")
+    print("{}".format(chr(number)), end="")
