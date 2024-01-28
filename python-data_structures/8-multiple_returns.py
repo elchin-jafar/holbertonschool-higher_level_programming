9dbc496a323c362f1faba6540324cb6cb9496e3d
Elchin <elcinceferli@gmail.com>
2024-01-28 11:21:39 -0800

    fix bug on empty sentence

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_letter = sentence[0]
     if not sentence:
-        first_letter = None
-    return (length, first_letter)
+        return (0, None)
+    return (len(sentence), sentence[0])
