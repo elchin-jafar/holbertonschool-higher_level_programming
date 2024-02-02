2c882fc7c90c68fe9f174585316b77e6f6496c67
Elchin <elcinceferli@gmail.com>
2024-02-02 13:22:09 -0800

    change if logic

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -5,7 +5,8 @@ def common_elements(set_1, set_2):
     list2 = list(set_2)
     res = list()
 
-    for i in range(len(list1)):
-        if (len(list1) != 0 and len(list2) != 0 and list1[i] == list2[i]):
-            res.append(list1[i])
+    for item in list1:
+        if item in list2:
+            res.append(item)
+
     return res
