ad6b1e07278f06b88c67993160b70e75c2f34643
Elchin <elcinceferli@gmail.com>
2024-02-03 07:59:11 -0800

    change logic

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,4 +4,8 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     max_value = max(a_dictionary.values())
-    return [key for key, value in a_dictionary.items() if value == max_value]
+    res = [key for key, value in a_dictionary.items() if value == max_value]
+    if res:
+        return res[0]
+    else:
+        return None
