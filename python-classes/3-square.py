6a7052bb64a9732d5a59e18438e3f65947d7456c
Elchin <elcinceferli@gmail.com>
2024-02-04 10:27:56 -0800

    area of square

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""method calcs area of square"""
+
+
+class Square:
+    """body of class"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
--- python-classes/README.md
+++ python-classes/README.md
@@ -2,3 +2,4 @@
 0 - create simple class <br />
 1 - class with instance private field <br />
 2 - class with instance private field and check if field has normal value and type on instantiate if not raise your hands <br />
+3 - public instance method which calcs area of square <br />
