9add20bb07e47398e97b15adc2c0a1f3777c7ddf
Elchin <elcinceferli@gmail.com>
2024-02-04 10:52:28 -0800

    print square

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""print square with # size of Square"""
+
+
+class Square:
+    """body of class"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
--- python-classes/README.md
+++ python-classes/README.md
@@ -4,3 +4,4 @@
 2 - class with instance private field and check if field has normal value and type on instantiate if not raise your hands <br />
 3 - public instance method which calcs area of square <br />
 4 - implement setter and getter to size field <br />
+5 - print square with # size of Square instance <br />
