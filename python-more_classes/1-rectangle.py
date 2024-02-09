69a4e5563acc1fe61366936e74d789ede35235bb
Elchin <elchin@Elchins-MacBook-Air.local>
2024-02-10 01:43:09 +0400

    add docs

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,16 +4,19 @@
 
 
 class Rectangle:
+    """Simple Rectangle with width and height attributes"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """getter for private width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter for private width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -22,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """getter for private height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for private height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
