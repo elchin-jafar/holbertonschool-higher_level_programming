1d224f981c9f671ba1463b5c7bccc6b86dfa5231
Elchin <elchin@Elchins-MacBook-Air.local>
2024-02-09 22:44:23 +0400

    simple rectange

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Representing Simple Rectangle"""
+
+
+class Rectangle:
+	"""Simple Rectangle"""
+	pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,2 @@
+## Python - More Classes and Objects <br />
+0 - Simple rectange <br />
