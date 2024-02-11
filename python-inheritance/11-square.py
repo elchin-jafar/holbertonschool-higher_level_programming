5aad2ac77aa5a8eed4bd85333b9544dd5c45b725
Elchin <elchin@Elchins-MacBook-Air.local>
2024-02-11 18:51:17 +0400

    update 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,10 +8,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """square class based on rectangle"""
+
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        return "[{}] {}/{}".format(self.__class__.__name__, self.__size, self.__size)
