a25da297a8a102afb917a55b06b0ea00ffbbffa0
Elchin <elchin@Elchins-MacBook-Air.local>
2024-02-11 18:52:12 +0400

    change str logic

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -19,4 +19,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
