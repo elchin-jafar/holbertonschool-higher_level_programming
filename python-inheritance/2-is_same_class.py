1a11455cb63b7028ff5ce61b7d29a4f29de5095b
Elchin <elchin@Elchins-MacBook-Air.local>
2024-02-11 16:21:49 +0400

    check types

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""checking is obj same type with  a_class"""
+
+
+def is_same_class(obj, a_class):
+    """check if given obj is same type with of a_class"""
+
+    if type(obj) == a_class:
+        return True
+    return False
