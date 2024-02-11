d42828918b032d6b03eae10e9982258ff062cd32
Elchin <elchin@Elchins-MacBook-Air.local>
2024-02-11 16:32:30 +0400

    check if obj isinstance of given class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Checking if instance or is inherited"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        returns true if obj is instance of a_class,
+        or false
+    """
+
+    return isinstance(obj, a_class)
