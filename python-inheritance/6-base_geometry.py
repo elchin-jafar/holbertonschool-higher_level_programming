68b323900ff56ee650bc4b33a46de5ccc8f2fd1c
Elchin <elchin@Elchins-MacBook-Air.local>
2024-02-11 17:20:52 +0400

    raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""empty class"""
+
+
+class BaseGeometry:
+    """empty class that doing nothing"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
