8a61bb80c1c4fabbf43dea90b5d3ca78b6c079bf
Elchin <elcinceferli@gmail.com>
2024-02-16 00:16:32 +0400

    update object

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,17 @@ class Rectangle(Base):
         width = self.width
         height = self.height
         return "[{}] ({}) {}/{} - {}/{}".format(name, id, x, y, width, height)
+
+    def update(self, *args):
+        """update instances"""
+
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
