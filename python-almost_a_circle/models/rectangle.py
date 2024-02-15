9eddfc7e6e4d28d7189b64b0cd67995a9c984c6c
Elchin <elcinceferli@gmail.com>
2024-02-15 23:43:12 +0400

    consider x and y on printing rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,8 +89,11 @@ class Rectangle(Base):
 
     def display(self):
         """displays rectangle with #"""
-
+        for y in range(self.y):
+            print("")
         for h in range(self.height):
+            for x in range(self.x):
+                print(" ", end="")
             for w in range(self.width):
                 print("#", end="")
             print("")
