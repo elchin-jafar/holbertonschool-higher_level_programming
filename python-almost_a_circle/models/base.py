49f809c6938253cf60bb77149ff7c397939a8d50
Elchin <elcinceferli@gmail.com>
2024-02-16 22:52:41 +0400

    serialize json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 
 """placeholder"""
 
+import json
+
 
 class Base:
     """base class"""
@@ -16,3 +18,12 @@ class Base:
         else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """convert list of dictionary to json string (serialize)"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
