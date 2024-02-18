cddd2f435bc32d48986aed3978920969621aacae
Elchin <elcinceferli@gmail.com>
2024-02-18 23:38:09 +0400

    add square tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,6 +4,7 @@ import unittest
 import sys
 from io import StringIO
 from models.rectangle import Rectangle
+import os
 
 
 class TestRectangle(unittest.TestCase):
@@ -80,6 +81,76 @@ class TestRectangle(unittest.TestCase):
         printed_output = captured_output.getvalue()
         sys.stdout = sys.__stdout__
         self.assertEqual(printed_output, "##\n##\n")
+
+    def test_to_dictionary(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        dict = { 'width': 1, 'height': 2, 'x': 3, 'y': 4, 'id': 5 }
+        self.assertEqual(r.to_dictionary(), dict)
+
+    def test_update_args(self):
+        r = Rectangle(1, 1, 1, 1, 1)
+        r.update()
+        self.assertEqual("[Rectangle] (1) 1/1 - 1/1", str(r))
+        r.update(2)
+        self.assertEqual("[Rectangle] (2) 1/1 - 1/1", str(r))
+        r.update(3, 3)
+        self.assertEqual("[Rectangle] (3) 1/1 - 3/1", str(r))
+        r.update(4, 4, 4)
+        self.assertEqual("[Rectangle] (4) 1/1 - 4/4", str(r))
+        r.update(5, 5, 5, 5)
+        self.assertEqual("[Rectangle] (5) 5/1 - 5/5", str(r))
+        r.update(6, 6, 6 ,6 ,6)
+        self.assertEqual("[Rectangle] (6) 6/6 - 6/6", str(r))
+
+    def test_update_kwargs(self):
+        r = Rectangle(1, 1, 1, 1, 1)
+        r.update(id=2)
+        self.assertEqual("[Rectangle] (2) 1/1 - 1/1", str(r))
+        r.update(id=3, width=3)
+        self.assertEqual("[Rectangle] (3) 1/1 - 3/1", str(r))
+        r.update(id=4, width=4, height=4)
+        self.assertEqual("[Rectangle] (4) 1/1 - 4/4", str(r))
+        r.update(id=5, width=5, height=5, x=5)
+        self.assertEqual("[Rectangle] (5) 5/1 - 5/5", str(r))
+        r.update(id=6, width=6, height=6, x=6, y=6)
+        self.assertEqual("[Rectangle] (6) 6/6 - 6/6", str(r))
+
+    def test_save_to_file(self):
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+        
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+        
+        
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+        
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+        
+        
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+        
+        Rectangle.save_to_file([Rectangle(1, 2, id=1)])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 1, "width": 1, "height": 2, "x": 0, "y": 0}]')
+
+    def test_load_from_file(self):
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+        self.assertEqual(Rectangle.load_from_file(), [])
 
 if __name__ == "__main__":
     unittest.main()
---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """test square class"""
+
+    def test_instanciate_works(self):
+        s = Square(2)
+        self.assertEqual(s.size, 2)
+
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+            Square()
+
+    def test_one_arg(self):
+        s1 = Square(10)
+        s2 = Square(11)
+        self.assertEqual(s1.id, s2.id - 1)
+
+    def test_two_args(self):
+        s1 = Square(10, 2)
+        s2 = Square(2, 10)
+        self.assertEqual(s1.id, s2.id - 1)
+
+    def test_three_args(self):
+        s1 = Square(10, 2, 2)
+        s2 = Square(2, 2, 10)
+        self.assertEqual(s1.id, s2.id - 1)
