4a793e1b7bd9935343d1eea765ebbdc6ec12a018
Elchin <elcinceferli@gmail.com>
2024-02-18 06:04:27 +0400

    check if instance has id field

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,11 +2,10 @@ import unittest
 
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
     """test base class from models module"""
 
-    def test_increment_id_on_init(self):
+    def test_id_exists(self):
         b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
+        self.assertTrue(b1.id)
