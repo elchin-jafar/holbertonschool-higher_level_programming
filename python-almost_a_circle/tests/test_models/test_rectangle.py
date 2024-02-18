94da43fbbeb569d5cb645990a18a8b2b276ce1a5
Elchin <elcinceferli@gmail.com>
2024-02-18 18:49:46 +0400

    add more tests to rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,3 +11,43 @@ class TestRectangle(unittest.TestCase):
         r = Rectangle(1, 2)
         self.assertEqual(r.width, 1)
         self.assertEqual(r.height, 2)
+
+    def test_rectangle_typeError(self):
+        with self.assertRaises(TypeError):
+            Rectangle('1')
+        with self.assertRaises(TypeError):
+            Rectangle(1, '2')
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, '3')
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, '4')
+
+    def test_rectangle_valueError(self):
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(2, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, -4)
+        
+    def test_rectangle_work_on_every_argument_pass(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 4)
+        self.assertEqual(r.id, 5)
+
+    def test_area(self):
+        r = Rectangle(2, 3)
+        self.assertEqual(r.area(), 6)
+
+    def test_str(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r.__str__(), "[Rectangle] (5) 3/4 - 1/2") 
