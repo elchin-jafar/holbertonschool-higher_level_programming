519b13345f11386d01a22e47670a269c5f500c9f
root <elcinceferli@gmail.com>
2024-03-24 15:02:23 +0000

    add full table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- create new table
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table
+VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
