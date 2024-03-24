ef98b58ae4d885685db8742a227cffabd0c6183e
root <elcinceferli@gmail.com>
2024-03-24 17:19:25 +0000

    grant select permission

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- create new db and new user which has only select privilege on new db
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
