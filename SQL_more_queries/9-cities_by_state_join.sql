53d4a9b44fa738f2e1783475d335e3f558950c5e
root <elcinceferli@gmail.com>
2024-03-24 18:23:10 +0000

    fix bug

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- list records using 2 table data
 SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.state_id = cities.id
-ORDER BY cities.id;
+WHERE cities.state_id = states.id;
