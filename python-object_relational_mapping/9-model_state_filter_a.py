25a1a767c0dd4486a1ed1dc7577d0a1997846330
root <elcinceferli@gmail.com>
2024-03-24 19:29:24 +0000

    Contains

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    lists all State objects that contain the letter
+    a from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        for state in session.query(State).filter(State.name.contains("a")):
+            print("{}: {}".format(state.id, state.name))
+    except Exception:
+        pass
