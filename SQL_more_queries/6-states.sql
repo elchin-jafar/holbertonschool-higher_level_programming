960c76d64dd45bc41b9bf17fe0a685772b6c0b72
root <elcinceferli@gmail.com>
2024-03-24 17:38:22 +0000

    fix bug

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- create new db and new table inside this db named according something with usa and states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
